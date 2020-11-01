3bfa0daa6df2e38d65eb3b65509f9e26e597c1d3
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-11-01 23:08:37 +0000

    Add typewriting animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Landing = () => {
+  const typeMe = useRef();
+
+  useEffect(() => {
+    const texts = "I'd like to make things.";
+    let index = 0;
+    let letter = '';
+
+    function type() {
+      letter = texts.slice(0, index++);
+      typeMe.current.textContent = letter;
+      const timerRef = setTimeout(type, 150);
+
+      if (letter.length === 24) {
+        window.clearTimeout(timerRef);
+      }
+    }
+
+    type();
+  }, []);
+
   return (
     <div className="landing wrapper">
       <div className="texts">
@@ -8,7 +28,7 @@ const Landing = () => {
         <h1 data-aos="fade-right" data-aos-delay="100">
           <div className="highlight">I'm Jinyoung,</div>
         </h1>
-        <h1 data-aos="fade-right" data-aos-delay="150" className="h1 typing js-typing"></h1>
+        <h1 data-aos="fade-right" data-aos-delay="150" ref={typeMe} className="type-me"></h1>
       </div>
 
       <a href="#projects" className="scroll">
