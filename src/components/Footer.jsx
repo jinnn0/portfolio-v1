a11e4844dbe1be5d3015df9ff80a23674ade2914
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-11-03 14:56:31 +0000

    Remove gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
         <a href="https://www.linkedin.com/in/jinyoung-jeong-457562140/" rel="noreferrer" target="_blank">
           LinkedIn
         </a>
-        <a href="/" rel="noreferrer" target="_blank">
+        {/* <a href="/" rel="noreferrer" target="_blank">
           Resume
-        </a>
+        </a> */}
       </div>
       <div>&copy;{new Date().getFullYear()} Jinyoung Jeong</div>
     </footer>
