581882a7f4302f3db0886fcfe94ba3ca34bf0e71
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-11-21 23:49:11 +0000

    Remove 100vh mobile fix - not necessary

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,6 @@ import About from './components/About';
 import Footer from './components/Footer';
 
 function App() {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight
-  });
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    }
-
-    let vh = windowSize.height * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  });
-
   return (
     <div className="App">
       <Landing />
